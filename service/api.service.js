656d0b63e5ef85a272104475a2f1589621bf18b9
Lukesh25092002 <lukeshpatil05@gmail.com>
2025-02-27 18:55:50 +0530

    api.service.js: new service implementation as an abstraction for connecting with backend

---
+++ service/api.service.js
@@ -0,0 +1,33 @@
+
+const block_emails = function (payload) {
+    let emails = data.emails;
+    const domain = data.domain;
+    const action = data.action;
+
+    if (domain)
+        emails = emails.map(email => '@' + email.split('@')[1]);
+
+    console.log("The emails are blocked:", emails);
+
+    fetch("http://localhost:8080/filter/create", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            emails: emails,
+            action: action
+        })
+    })
+        .then((res) => {
+            return res.json();
+        })
+        .then((data) => {
+            response({ success: true, data });
+        })
+        .catch((err) => {
+            response({ success: false, err });
+        });
+}
+
+export default {
+    block_emails
+};
